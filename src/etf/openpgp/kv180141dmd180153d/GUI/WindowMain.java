e0fa1381ae2f312294d7ca98a8715c8d0c3044ae
Viktor <viktorkatz619@gmail.com>
2022-05-27 19:18:02 +0200

    Added import key button

--- src/etf/openpgp/kv180141dmd180153d/GUI/WindowMain.java
+++ src/etf/openpgp/kv180141dmd180153d/GUI/WindowMain.java
@@ -1,5 +1,6 @@
 package etf.openpgp.kv180141dmd180153d.GUI;
 
+import java.io.File;
 import java.util.Vector;
 
 import javax.swing.*;
@@ -16,8 +17,8 @@ public class WindowMain extends JFrame {
 		if (instance == null)
 			instance = new WindowMain();
 		return instance;
-	}	
-	
+	}
+
 	private final static String windowTitle = "ZP homework, Viktor i Dragan";
 	private final static int windowX = 800;
 	private final static int windowY = 800;
@@ -38,14 +39,14 @@ public class WindowMain extends JFrame {
 	private PrivateKeyRingPanel privateKeyRingTab = new PrivateKeyRingPanel(privateKeys);
 	private PublicKeyRingPanel publicKeyRingTab = new PublicKeyRingPanel(publicKeys);
 	private JPanel buttonPanel = new JPanel();
-	
+
 	private WindowMain() {
 		tabsPane.setBounds(0, 0, windowX, horizontalBreak);
 		tabsPane.add("Private Key Ring", privateKeyRingTab);
 		tabsPane.add("Public Key Ring", publicKeyRingTab);
 
 		addButtons();
-		
+
 		this.add(tabsPane);
 		this.add(buttonPanel);
 
@@ -57,47 +58,62 @@ public class WindowMain extends JFrame {
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
-	
+
 	private void addButtons() {
 		JButton newKeyPairButton = new JButton("Create new key pair");
 		newKeyPairButton.addActionListener(e -> new WindowNewKeyPair(this));
 		buttonPanel.add(newKeyPairButton);
 		buttonPanel.setBounds(0, horizontalBreak, windowX, windowY - horizontalBreak);
-		
+
 		JButton deleteSelectedKeysButton = new JButton("Delete selected keys");
 		deleteSelectedKeysButton.addActionListener(e -> deleteSelectedKeys());
 		buttonPanel.add(deleteSelectedKeysButton);
-		
+
 		JButton encryptMessageButton = new JButton("Send message");
 		encryptMessageButton.addActionListener(e -> new WindowEncryptMessage(this, publicKeys, privateKeys));
 		buttonPanel.add(encryptMessageButton);
+
+		JButton importKeyButton = new JButton("Import foreign private key");
+		importKeyButton.addActionListener(e -> importPublicKey());
+		buttonPanel.add(importKeyButton);
 	}
-	
+
 	public void loadDataFromDisk() {
 		throw new NotImplementedException();// TODO viktor: Pozovi Draganove funkcije kad ih napravi
 		// refreshData();
 	}
-	
-	public void saveDataToDisk() {	// Ovo ce se pozivati kad se izvrse neke bitne akcije, tipa pravljenje kljuca, brisanje, itd.
+
+	public void saveDataToDisk() { // Ovo ce se pozivati kad se izvrse neke bitne akcije, tipa pravljenje kljuca, brisanje, itd.
 		throw new NotImplementedException();// TODO viktor: Pozovi Draganove funkcije kad ih napravi
 	}
 
-	public void refreshData() {
+	public void refreshTables() {
 		privateKeyRingTab.refreshData();
 		publicKeyRingTab.refreshData();
 	}
-	
+
 	private void deleteSelectedKeys() {
 		KeyRingPanel selectedTab = (KeyRingPanel) tabsPane.getSelectedComponent();
 		int[] selectedKeys = selectedTab.getSelectedKeys();
-		
+
 		System.out.print("(TODO) Selected indexes of keys for deletion: ");
-		for(int sk : selectedKeys)
+		for (int sk : selectedKeys)
 			System.out.print(" " + sk);
 		System.out.println();
-		
+
 		throw new NotImplementedException(); // TODO @viktor: kad dragan uradi kljuceve, izbrisi selektovane
 		// saveDataToDisk(); // Flush data
+	}
+
+	private void importPublicKey() {
+		JFileChooser keyFileChooser = new JFileChooser(".");
+		if (JFileChooser.APPROVE_OPTION == keyFileChooser.showOpenDialog(this)) {
+			File selectedFile = keyFileChooser.getSelectedFile();
+
+			// TODO @viktor: Nakon sto Dragan doda ucitavanje iz .asc fajla, pozovi to
+
+			refreshTables();
+		}
 	}
 
 	public static void main(String[] args) {
